174eadd478b55996f04f01c2bb4916406e871981
Danilo Augusto <dandevweb@gmail.com>
2024-08-03 11:44:45 -0300

    test: contacts list

--- tests/Feature/Livewire/Contacts/IndexTest.php
+++ tests/Feature/Livewire/Contacts/IndexTest.php
@@ -1,17 +1,67 @@
 <?php
 
-use App\Models\User;
 use Livewire\Livewire;
+use App\Models\{Contact, User};
 use App\Livewire\Contacts\Index;
+use Illuminate\Pagination\LengthAwarePaginator;
 
-use function Pest\Laravel\actingAs;
+use function Pest\Laravel\{actingAs, get};
 
 beforeEach(function () {
     $this->user = User::factory()->create();
     actingAs($this->user);
 });
 
-it('renders successfully', function () {
+it('renders contact list successfully', function () {
+    Livewire::test(Index::class)->assertOk();
+    get(route('contacts'))->assertOk();
+});
+
+test("let's create a livewire component to list all contacts in the page", function () {
+    $contacts = Contact::factory()->count(10)->create();
+
+    $lw = Livewire::test(Index::class);
+
+    $lw->assertSet('contacts', function ($items) {
+        expect($items)
+        ->toHaveCount(10);
+
+        return true;
+    });
+
+    foreach ($contacts as $contact) {
+        $lw->assertSee($contact->name);
+        $lw->assertSee($contact->email);
+        $lw->assertSee($contact->phone);
+        $lw->assertSee($contact->city);
+    }
+});
+
+it('should be able to paginate the result', function () {
+    Contact::factory(30)->create();
+
     Livewire::test(Index::class)
-        ->assertStatus(200);
+        ->assertSet('contacts', function (LengthAwarePaginator $items) {
+            expect($items)
+                ->toHaveCount(10);
+
+            return true;
+        });
+
+});
+
+it('should display contacts across multiple pages', function () {
+    Contact::factory()->count(30)->create();
+
+    $lw = Livewire::test(Index::class)
+        ->call('gotoPage', 2);
+
+    $contacts = Contact::latest()->skip(10)->take(10)->get();
+
+    foreach ($contacts as $contact) {
+        $lw->assertSee($contact->name);
+        $lw->assertSee($contact->email);
+        $lw->assertSee($contact->phone);
+        $lw->assertSee($contact->city);
+    }
 });
